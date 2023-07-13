b5ca45252d5b3275d9a111cc1afdf09d8c62fa6e
Bittu Chauhan <bittuchauhan10@gmail.com>
2023-07-13 04:51:16 +0200

    Cleanup

--- WebUI/documentation/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from documentation import views
-
-urlpatterns = [
-    url(r"^$", views.index, name="documentation-index"),
-]
--- WebUI/log_management/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from log_management import views
-
-urlpatterns = [
-    url(r"^$", views.index, name="index"),
-    url(r"^setlog/([a-zA-Z0-9._ -]+)/$", views.set_log, name="set_log"),
-    url(r"^ajax/loginfo/$", views.get_log_info, name="get_log_info"),
-]
